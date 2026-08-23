e4119d7167c47e97d84d23bf189a1bbb8df2233c
balikrotasi1041 <balikrotasi1041@gmail.com>
2026-08-23 21:37:42 +0300

    Export service rights guide

--- src/ui.js
+++ src/ui.js
@@ -1,2 +1,3 @@
 export { renderHome,renderDeviceType,renderBrand,renderFamily,renderModel } from "./ui-hubs.js";
 export { renderIssue,renderSearch,renderPolicy,render404 } from "./ui-issue.js";
+export { renderServiceRights } from "./ui-service.js";
