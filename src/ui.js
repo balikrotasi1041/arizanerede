aa4fc3a442abff70d97fee1c28a6e7023913b33c
balikrotasi1041 <balikrotasi1041@gmail.com>
2026-09-02 23:32:26 +0300

    Activate enhanced hub and SEO page renderers

--- src/ui.js
+++ src/ui.js
@@ -1,3 +1,4 @@
-export { renderHome,renderDeviceType,renderBrand,renderFamily,renderModel } from "./ui-hubs.js";
+export { renderHome,renderDeviceType,renderBrand,renderFamily,renderModel } from "./ui-hubs-v2.js";
 export { renderIssue,renderSearch,renderPolicy,render404 } from "./ui-issue.js";
 export { renderServiceRights } from "./ui-service.js";
+export { renderEditorialGuide,renderServiceGuide,pathForEditorialGuide,pathForServiceGuide } from "./ui-seo.js";
