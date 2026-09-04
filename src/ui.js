541c50c65dd1a23d085579b31e3f4b8bca631bc4
balikrotasi1041 <balikrotasi1041@gmail.com>
2026-09-04 23:43:42 +0300

    Activate repair-intent V3 hubs

--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,4 @@
-export { renderHome,renderDeviceType,renderBrand,renderFamily,renderModel } from "./ui-hubs-v2.js";
+export { renderHome,renderDeviceType,renderBrand,renderFamily,renderModel } from "./ui-hubs-v3.js";
 export { renderIssue,renderSearch,renderPolicy,render404 } from "./ui-issue.js";
 export { renderServiceRights } from "./ui-service.js";
 export { renderEditorialGuide,renderServiceGuide,pathForEditorialGuide,pathForServiceGuide } from "./ui-seo.js";
